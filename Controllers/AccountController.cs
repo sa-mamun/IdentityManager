3191cc1d64460b4e2148b1e831e0d54ca0151183
samir <samamun009@gmail.com>
2024-01-31 00:50:41 +0600

    Login logic code added

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -50,8 +50,27 @@ namespace IdentityManager.Controllers
 
         public IActionResult Login()
         {
-
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                }
+            }
+
+            return View(model);
         }
 
         [HttpPost]
